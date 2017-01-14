bcf628ca998d1bcdd42e5f3f89da4f473d4d16fe
Omar Ashour <omar.ashour22@gmail.com>
2017-01-13 20:45:03 -0600

    Add support for pulling avatars in png

--- habitica/api.py
+++ habitica/api.py
@@ -51,11 +51,16 @@ class Habitica(object):
             direction = kwargs.pop('_direction', None)
             uri = '%s/%s' % (self.auth['url'],
                              API_URI_BASE)
-            if aspect_id is not None:
+            if self.aspect == 'avatar':
+                uri = '%s/%s/%s-%s.png' % (self.auth['url'], 
+                                           self.resource,
+                                           self.aspect,
+                                           str(aspect_id))
+            elif aspect_id is not None:
                 uri = '%s/%s/%s/%s' % (uri,
-                                    self.aspect,
-                                    str(aspect_id),
-                                    self.resource)
+                                       self.aspect,
+                                       str(aspect_id),
+                                       self.resource)
                 if resource_id is not None:
                     uri = '%s/%s' % (uri,
                                      str(resource_id))
@@ -86,8 +91,9 @@ class Habitica(object):
             res = getattr(requests, method)(uri, headers=self.headers,
                                             params=kwargs)
 
-        # print(res.url)  # debug...
+        print(res.url)  # debug...
         if res.status_code == requests.codes.ok or requests.codes.created:
+            return res
             if "data" in res.json():
                 return res.json()["data"]
             else:
